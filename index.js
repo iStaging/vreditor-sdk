460094a3d869f02c844d87c037629ffe3c6c5ec9
Kevin Hu <sky790312yks@gmail.com>
2018-05-10 11:49:14 +0800

    feat: add onCreatePanoramas, onDeletePanoramas, onUploadPanoramas, onSortPanoramas, onSaveMarkerSuccess, onSaveMarkerError callback with data for user

--- index.js
+++ index.js
@@ -12,3 +12,27 @@ app.init({
   el: '#vreditor-sdk',
   lang: 'zh-cn'
 })
+
+app.onCreatePanoramas = (data) => {
+  console.log('onCreatePanoramas: ', data)
+}
+
+app.onDeletePanoramas = (data) => {
+  console.log('onDeletePanoramas: ', data)
+}
+
+app.onUpdatePanorama = (data) => {
+  console.log('onUpdatePanorama: ', data)
+}
+
+app.onSortPanoramas = (data) => {
+  console.log('onSortPanoramas: ', data)
+}
+
+app.onSaveMarkerSuccess = (data) => {
+  console.log('onSaveMarkerSuccess: ', data)
+}
+
+app.onSaveMarkerError = (data) => {
+  console.log('onSaveMarkerError: ', data)
+}
